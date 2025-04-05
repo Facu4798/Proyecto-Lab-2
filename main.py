cccc8eaa317b3742de9d82a2bfb327dbaa65afff
Facundo Mazzola <facumazzola2@gmail.com>
2025-04-05 22:27:41 +0000

    Se agrega la obtencion automatica de la fecha actual

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from buscar_prediccion import buscar_prediccion
 #input
 dias = input("a cuantos dias quiere estimar el riesgo?")
 
+from datetime import datetime
+fecha = datetime.now().strftime('%d-%m-%Y')
+
 #buscar la predicción en la base de datos
 prediccion = buscar_prediccion(fecha,dias)
 
