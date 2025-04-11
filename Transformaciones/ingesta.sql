72ed003ba0dbb254ca2408bc2e2b35332526058e
Facundo Mazzola <facumazzola2@gmail.com>
2025-04-11 12:18:14 +0000

    sql para FE

---
+++ Transformaciones/ingesta.sql
@@ -0,0 +1,14 @@
+select *,
+    (open-close) as retorno,
+    abs((open-close)/open) as variacion
+from stock_data
+inner join (
+    select case 
+               when count(*) over (order by date rows between current row and 10 following) < 10 
+               then 0 
+               else min(Close) over (order by date rows between current row and 10 following) 
+           end as target,
+           date
+    from stock_data)
+    on stock_data.date = stock_data.date
+    
