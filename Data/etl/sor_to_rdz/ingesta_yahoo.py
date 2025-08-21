1b5a8ca18ad3969871d7c1d8fbf56a8b4669cf89
Facundo Mazzola <facumazzola2@gmail.com>
2025-08-21 02:27:53 +0000

    try

---
+++ Data/etl/sor_to_rdz/ingesta_yahoo.py
@@ -0,0 +1,36 @@
+def obtener_datos_yahoo(start=None, end=None,
+                        ticker='TSLA'):
+
+    #importar pandas
+    try:
+        import pandas as pd
+    except:
+        import os
+        os.system('pip install pandas')
+        import pandas as pd
+
+    #importar yahoo finance de yfinance
+    try:
+        import yfinance as yf
+    except:
+        import os
+        os.system('pip install yfinance --upgrade')
+        import yfinance as yf
+
+    # Descargar datos de Yahoo Finance
+    try:
+        # Descargar datos de Yahoo Finance
+        data = yf.download(ticker, start=start, end=end,interval='1d')
+        if data.empty:
+            raise ValueError("No data found for the given date.")
+        
+        # Eliminar la segunda fila de columnas
+        data.columns = data.columns.droplevel(1)
+        data.columns = [i for i in data.columns]
+        data.index = [i for i in data.index]
+
+        #ver lo del ajclose
+        return data
+    except Exception as e:
+        print(f"Error downloading data: {e}")
+        return None
