6a5bd656ef635dccca97ff7a4f3153d667276db5
Facundo Mazzola <facumazzola2@gmail.com>
2025-08-28 22:48:31 +0000

    added try catch on ingesta.py

--- Data/etl/sor_to_rdz/ingesta.py
+++ Data/etl/sor_to_rdz/ingesta.py
@@ -4,25 +4,35 @@ from la_libreria.authentication import Credentials
 import os
 os.system("clear")
 
-creds = Credentials().load(path="Credentials/db_prod.json")
+try:
+    creds = Credentials().load(path="Credentials/db_prod.json")
+except:
+    raise Exception("No se pudo cargar las credenciales")
 
 from carga_yahoo import cargar_datos_yahoo
 from carga_fred import cargar_datos_fred
 
-yahoo_date = get_cdc_date("sor_to_rdz ^GSPC")
-yahoo_date = substract_date(date_str = str(yahoo_date))
-fred_date = get_cdc_date("sor_to_rdz fred")
-fred_date = substract_date(date_str = str(fred_date))
+try:
+    yahoo_date = get_cdc_date("sor_to_rdz ^GSPC")
+    yahoo_date = substract_date(date_str = str(yahoo_date))
 
-cargar_datos_yahoo(
-    inicio=yahoo_date,
-    fin=None,
-    credentials=creds,
-    ticker="^GSPC"
-)
+    cargar_datos_yahoo(
+        inicio=yahoo_date,
+        fin=None,
+        credentials=creds,
+        ticker="^GSPC"
+    )
+except: pass
 
-cargar_datos_fred(
-    inicio=fred_date,
-    fin=None,
-    credentials=creds
-)
+try:
+    fred_date = get_cdc_date("sor_to_rdz fred")
+    fred_date = substract_date(date_str = str(fred_date))
+
+
+    cargar_datos_fred(
+        inicio=fred_date,
+        fin=None,
+        credentials=creds
+    )
+except:
+    pass
