927a6eeeb3ede2e3779eea970c32aba8eabe469e
Facundo Mazzola <facumazzola2@gmail.com>
2025-10-07 20:29:19 +0000

    renamed folder

--- Data/data_pipe.py
+++ Data/data_pipe.py
@@ -1,18 +1,44 @@
 import os
 import sys
 
+
+# script_paths = [
+#        "/workspaces/Proyecto-Lab-2/Data/etl/sor_to_rdz/ingesta.py",
+#        "/workspaces/Proyecto-Lab-2/Data/etl/rdz_to_cdz/curado.py",
+#        "/workspaces/Proyecto-Lab-2/src/predictions & training/predecir.py"
+#     ]
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+current_dir = current_dir.rsplit('Proyecto-Lab-2', 1)[0] + 'Proyecto-Lab-2/'
+os.chdir(current_dir)
+print(current_dir)
+
+sys.path.insert(0, current_dir)
+
+
+from la_libreria.authentication import Credentials
+from la_libreria.connectors import MySQLConnector
+
+from ..Data.etl.sor_to_rdz.ingesta import *
+from ..Data.etl.rdz_to_cdz.curado import *
+from ..src.predictions_and_training.predecir import *
+
+sys.path.pop(0)
+
+
+
+
+
+
+
+
+
 def data_pipe():
     import importlib.util
 
-    script_paths = [
-       "/workspaces/Proyecto-Lab-2/Data/etl/sor_to_rdz/ingesta.py",
-       "/workspaces/Proyecto-Lab-2/Data/etl/rdz_to_cdz/curado.py",
-       "/workspaces/Proyecto-Lab-2/src/predictions & training/predecir.py"
-    ]
-
     for script_path in script_paths:
         script_dir = os.path.dirname(script_path)
-        sys.path.insert(0, script_dir)  # Add script's directory to sys.path
+          # Add script's directory to sys.path
 
         if not os.path.isfile(script_path):
             print(f"File not found: {script_path}")
@@ -32,5 +58,3 @@ def data_pipe():
             print(f"Error executing {script_path}: {e}")
         finally:
             sys.path.pop(0)  # Clean up sys.path
-
-data_pipe()
