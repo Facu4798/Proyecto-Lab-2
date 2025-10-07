8624fbd5ce36abe9122b2bd478fd0b31c5a1546e
Facundo Mazzola <facumazzola2@gmail.com>
2025-10-07 21:26:32 +0000

    no comment

--- Data/data_pipe.py
+++ Data/data_pipe.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
 current_dir = current_dir.rsplit('Proyecto-Lab-2', 1)[0] + 'Proyecto-Lab-2/'
 os.chdir(current_dir)
@@ -10,14 +8,15 @@ print(current_dir)
 
 sys.path.insert(0, current_dir)
 
-
+#generar la conexion a la base de datos
 from la_libreria.authentication import Credentials
 from la_libreria.connectors import MySQLConnector
 creds = Credentials().load(path="/workspaces/Proyecto-Lab-2/Credentials/db_dev.json")
 conn = MySQLConnector(creds.dict).connect()
+
+# ejecutar los procesos ETL
 from Data.etl.sor_to_rdz.ingesta import ingestar
 from Data.etl.rdz_to_cdz.curado import curar
-
 
 ingestar(conn=conn,creds=creds)
 curar(conn=conn,creds=creds)
