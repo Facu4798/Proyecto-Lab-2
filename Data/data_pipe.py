aff4beb4bbcae24dbf5097f2c8246b46b1c0525e
Facundo Mazzola <facumazzola2@gmail.com>
2025-10-07 22:28:01 +0000

    time registry

--- Data/data_pipe.py
+++ Data/data_pipe.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 current_dir = current_dir.rsplit('Proyecto-Lab-2', 1)[0] + 'Proyecto-Lab-2/'
@@ -11,15 +12,30 @@ sys.path.insert(0, current_dir)
 #generar la conexion a la base de datos
 from la_libreria.authentication import Credentials
 from la_libreria.connectors import MySQLConnector
+
+startup_time = time.time()
 creds = Credentials().load(path="/workspaces/Proyecto-Lab-2/Credentials/db_dev.json")
 conn = MySQLConnector(creds.dict).connect()
-
+startup_time = time.time() - startup_time
 # ejecutar los procesos ETL
 from Data.etl.sor_to_rdz.ingesta import ingestar
 from Data.etl.rdz_to_cdz.curado import curar
 
+
+ingest_start = time.time()
 ingestar(conn=conn,creds=creds)
+ingestion_time = time.time() - ingest_start
+
+
+curation_start = time.time()
 curar(conn=conn,creds=creds)
+curation_time = time.time() - curation_start
+
+os.system("clear")
+print(f"Tiempo de inicio y conexion a la base de datos: {startup_time:.2f} segundos.")
+print(f"Ingesta completada en {ingestion_time:.2f} segundos.")
+print(f"Curado completado en {curation_time:.2f} segundos.")
+print(f"Tiempo total de ejecucion: {startup_time + ingestion_time + curation_time:.2f} segundos.")
 
 sys.path.pop(0)
 
