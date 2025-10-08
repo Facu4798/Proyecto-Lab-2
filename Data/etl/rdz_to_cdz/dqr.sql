9738e3fe983755fd5cf6f8b7f8da947d00bea793
Facundo Mazzola <facumazzola2@gmail.com>
2025-10-08 20:15:20 +0000

    Added new dqr

--- Data/etl/rdz_to_cdz/dqr.sql
+++ Data/etl/rdz_to_cdz/dqr.sql
@@ -3,6 +3,11 @@ with stock_curated as(
     where Date <= CURRENT_DATE()
     and Date is not null
     and Ticker is not null
+    and Open >= 0
+    and Close >= 0
+    and High >= 0
+    and Low >= 0
+    and Volume >= 0
 ),
 macro_curated as(
     select * from macro_data
