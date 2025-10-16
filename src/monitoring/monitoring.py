9476e195b741f551f865966cb0f463886eb01717
Facundo Mazzola <facumazzola2@gmail.com>
2025-10-16 20:53:47 +0000

    finished securing codebase

--- src/monitoring/monitoring.py
+++ src/monitoring/monitoring.py
@@ -2,12 +2,15 @@ from la_libreria.authentication import Credentials
 from la_libreria.connectors import MySQLConnector
 from la_libreria.utils import get_ts
 import os
-os.system("clear")
+from la_libreria.utils import parse_query
+import dotenv
+dotenv.load_dotenv(dotenv.find_dotenv("moni.env"))
+# os.system("clear")
 import pandas as pd
 
 def metric_monitoring():
 
-    credentials = Credentials().load(path="Credentials/db_prod.json")
+    credentials = Credentials().load(path=os.getenv("DEV_CREDS"))
     conn = MySQLConnector(credentials.dict)
     conn.test_connection()
     conn.connect()
@@ -21,25 +24,10 @@ def metric_monitoring():
     })
 
     for t in [5,10,30]:
-        metric = conn.get_data(f"""                     
-        WITH pred_f AS(select * from predicciones where Ticker = '{Ticker}'),
-        cur_f AS(select * from curated where Ticker = '{Ticker}'),
-        pre AS (
-            SELECT c.date as curated_date, 
-                p.date as prediction_date,
-                c.Target{t} as real_value,
-                p.{t}Days prediction,
-                c.Ticker 
-            FROM cur_f c 
-            INNER JOIN pred_f p
-            ON c.Date = DATE_ADD(p.Date, INTERVAL {t} DAY)
-        )
-        SELECT MAX(pre.prediction_date) as Date, 
-        AVG(ABS(pre.prediction-pre.real_value)) as MAE,
-        '{Ticker}' AS Ticker, 
-        '{t}' as Days
-        FROM pre
-        """)
+        metric = conn.get_data(parse_query(
+            filepath=os.getenv("MODEL_TRACKING_SQL"),
+            replacement_dict={"ticker_placeholder": Ticker, "t_placeholder": t}
+        ))
         metric.dropna(inplace=True)
         if not metric.empty:
             metric_table = pd.concat([metric_table, metric], axis=0)
@@ -61,7 +49,5 @@ def metric_monitoring():
             pks=["Date", "Ticker", "Days"]
         )
         
-
-    print(conn.get_data("SELECT * FROM metric_table"))
 
     conn.close()
